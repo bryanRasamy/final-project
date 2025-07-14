0032e3d56603f14f72334ae3ef506f937c34643c
bryanRasamy <bryanrasamiaramanana@gmail.com>
2025-07-14 12:25:32 +0300

    6

--- pages/login.php
+++ pages/login.php
@@ -9,63 +9,63 @@
 </head>
 <body>
     <div class="container">
-        <div class="row head1 shadow">
+        <section class="row head1 shadow">
             <h1>Site d'emprunt d'objet</h1>
-        </div>
-        <div class="row mt-5 esp1">
-            <div class="col-3 col-sm-3 col-3 mt-5"></div>
-            <div class="col-6 col-sm-6 col-6 border shadow modif mt-5">
+        </section>
+        <section class="row mt-5 esp1">
+            <article class="col-3 col-sm-3 col-3 mt-5"></article>
+            <article class="col-6 col-sm-6 col-6 border shadow modif mt-5">
                 <h3 class="text-center">Veuillez vous connecter</h3>
                     <form action="traitementlogin.php" method="get" class="mt-4">
                         <?php if(!isset($_GET['a'])){ ?>
                             <label for="email" class="mt-4">Email:</label>
-                            <div class="input-group mb-3">
-                                <div class="form-floating">
+                            <section class="input-group mb-3">
+                                <article class="form-floating">
                                     <input type="email" class="form-control" name="email" id="floatingInputGroup1" placeholder="Email de l'utilisateur">
                                     <label for="floatingInputGroup1">Email</label>
-                                </div>
-                            </div>
+                                </artilce>
+                            </section>
 
                             <label for="email" class="mt-4">Mot de passe:</label>
-                            <div class="input-group mb-3">
-                                <div class="form-floating">
+                            <section class="input-group mb-3">
+                                <article class="form-floating">
                                     <input type="password" class="form-control" name="mdp" id="floatingInputGroup1" placeholder="Mot de passe de l'utilisateur">
                                     <label for="floatingInputGroup1">Mot de passe</label>
-                                </div>
-                            </div>
+                                </article>
+                            </section>
                         <?php }?>
                         <?php if(isset($_GET['a'])){ ?>
                             <label for="email">Email:</label>
-                            <div class="input-group has-validation">
-                                <div class="form-floating is-invalid">                    
-                                    <div class="form-floating is-invalid">                 
+                            <section class="input-group has-validation">
+                                <article class="form-floating is-invalid">                    
+                                    <section class="form-floating is-invalid">                 
                                         <input type="email" name="email" i class="form-control is-invalid" id="floatingInputGroup2"  placeholder="Email de l'utilisateur" required>
                                         <label for="floatingInputGroup2">Email</label>
-                                    </div> 
-                                </div>
-                            </div>
+                                    </section> 
+                                </article>
+                            </section>
                            
                             <label for="email" class="mt-4">Mot de passe:</label>
-                            <div class="input-group has-validation">
-                                <div class="form-floating is-invalid">                    
-                                    <div class="form-floating is-invalid">                 
+                            <section class="input-group has-validation">
+                                <article class="form-floating is-invalid">                    
+                                    <section class="form-floating is-invalid">                 
                                         <input type="text" name="mdp" id="pseudo" class="form-control is-invalid" id="floatingInputGroup2" placeholder="Entrez votre pseudo" required>
                                         <label for="floatingInputGroup2">mot de passe</label>
-                                    </div>
-                                    <div class="invalid-feedback">
+                                    </section>
+                                    <section class="invalid-feedback">
                                         Email ou mot de passe non valide !!
-                                    </div>  
-                                </div>
-                            </div>
+                                    </section>  
+                                </article>
+                            </section>
                         <?php }?>
-                        <div class="d-grid gap-2 mt-4">
+                        <section class="d-grid gap-2 mt-4">
                             <input type="submit" value="Connexion" id="valider" class="btn btn-primary">
                             <a href="inscription.php" class="link-danger mt-4 text-center">Pas encore de compte?</a>
-                        </div>
+                        </section>
                         
                 </form>
-            </div>
-        </div>
+            </article>
+        </section>
     </div>
     <script src=".././assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
